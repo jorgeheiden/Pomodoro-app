9d748726a3cfd4826bb0888f405a2312c4850194
jorge heidenreich <jmjhjorge@gmail.com>
2022-08-26 19:18:52 -0300

    problema con localStorage en android solucionado

--- src/app/componentes/tareas/tareas.component.ts
+++ src/app/componentes/tareas/tareas.component.ts
@@ -23,9 +23,10 @@ export class TareasComponent implements OnInit {
   ngOnInit(): void {
 
    this.obtenerLocalStorage = localStorage.getItem("tasks")
-    this.tareas = JSON.parse(this.obtenerLocalStorage)
+    this.tareas = JSON.parse(this.obtenerLocalStorage || '[]')
     console.log(this.obtenerLocalStorage)
     console.log(this.tareas)
+    
   }
 
   Agregar(){
@@ -41,7 +42,7 @@ export class TareasComponent implements OnInit {
     localStorage.setItem("tasks", JSON.stringify(this.tareas))
 
     this.obtenerLocalStorage = localStorage.getItem("tasks")
-    this.tareas = JSON.parse(this.obtenerLocalStorage)
+    this.tareas = JSON.parse(this.obtenerLocalStorage || '[]')
 
 
   }
